9817481663fd2ba103037bfee194cabca785e115
Leandro <camposweb@gmail.com>
2022-02-02 16:26:59 -0300

    Subindo Modificações

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
+import styles from './styles.module.scss';
+
+export function Header(){
+
+
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Link href="/">
+                    <a><img src="/images/logo.svg" alt="ig.news" /></a>
+                </Link>
+                <nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
